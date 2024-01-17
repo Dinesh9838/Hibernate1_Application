4d9b198b129ec0497c9e54ce8a59545c64539ff7
dinesh kumar <dinesh983892@gmail.com>
2024-01-17 10:29:06 +0530

    update

--- src/main/java/hibernate/client/App.java
+++ src/main/java/hibernate/client/App.java
@@ -6,79 +6,42 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
 import java.util.List;
 
 import hibernate.entity.Employee;
+import jakarta.persistence.TypedQuery;
 
 public class App {
 
+	@SuppressWarnings({ "deprecation", "unchecked" })
 	public static void main(String[] args) {
-//		Employee e = new Employee();
-//		Employee v = new Employee();
-//		e.setId(1);
-//		e.setName("kishan Kumar");
-//		e.setGender("male");
-//		e.setSalary(22000);
-//		
-//		v.setId(2);
-//		v.setName("shyam");
-//		v.setGender("male");
-//		v.setSalary(2500000);
-		
-//		Configuration cfg = new Configuration().configure();
-//		SessionFactory sf = cfg.buildSessionFactory();
-		
-		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure().build();
+
+		// Create typesafe ServiceRegistry object
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
 		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
-		SessionFactory sf = meta.buildSessionFactory();		
-		
+		SessionFactory sf = meta.getSessionFactoryBuilder().build();
 		Session session = sf.openSession();
 		Transaction tx = session.beginTransaction();
-		System.out.println("--Start----");
+		System.out.println("Staart...");
+		Employee e1 = new Employee();
+		List<Employee> employees= session.createNamedQuery("Employee.findEmployeeById",Employee.class)
+				.setParameter("id", 5)
+				.getResultList();
+		for (Employee employee : employees) {
+			System.out.println("Emp: "+employee);
+		}
+		System.out.println("---------------------------------------------------");
 		
-//		Example of HQL to get all the records
-//		Query query1 =session.createQuery("from Employee");
-//		List list=query1.list();
-//		System.out.println(list);
-//		Query q=session.createQuery("select max(name) from Employee");  
-//		List<Integer> list1=q.list();  
-//		System.out.println(list1.get(0));  
-//		
-////		HQL to get records with pagination
-//		Query query2=session.createQuery("from Employee");  
-//		query2.setFirstResult(5);  
-//		query2.setMaxResults(5);  
-//		List list2=query2.list();//will return the records from 5 to 10th number  
-//		System.out.println("5 to 10: "+list2);
-//		
-//		HQL update query Using Named Parameters
-		Query q=session.createQuery("update Employee set name=:n where id=:i");  
-		q.setParameter("n","Udit kumar");  
-		q.setParameter("i",1);  
-		System.out.println("status: "+q.executeUpdate());  
-
-//		HQL delete query
-		Query query=session.createQuery("delete from Employee where id= 20");  
-		query.executeUpdate();  
-		
-		Query query1=session.createQuery("from Employee");
-		List list=query1.list();  
-		System.out.println(list);
-		
-		
-//		session.save(e);
-//		session.save(v);
-//		System.out.println(e);
-//		System.out.println(v);
-		
-		
+//		List<Employee> emp= session.createNamedQuery("Employee.findByGender",Employee.class)
+//				.setParameter("gender", "male").getResultList();
+//
+//		for (Employee employee : emp) {
+//			System.out.println("Emp: "+employee);
+//		}
 		tx.commit();
 		session.close();
 		sf.close();
-		
-//		System.out.println("main_method");
-		}
+
+	}
 
 }
--- src/main/java/hibernate/entity/Employee.java
+++ src/main/java/hibernate/entity/Employee.java
@@ -1,43 +1,61 @@
 package hibernate.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+
+@NamedQuery(
+		name = "Employee.findEmployeeById", 
+		query = "FROM ducat_emp E WHERE E.id > :id"
+		)
+@NamedQuery(
+	    name = "Employee.findByGender",
+	    query = "SELECT e FROM ducat_emp e WHERE e.gender = :gender"
+	)
+
+
 
 @Entity(name = "ducat_emp")
-@Getter
-@Setter
-@ToString
 public class Employee {
-	private int id;
+	
+	@Column(name = "user_name")
 	private String name;
 	private String gender;
-	private int salary;
-	public int getId() {
-		return id;
-	}
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-	public int getSalary() {
-		return salary;
+
+	public int getId() {
+		return id;
 	}
-	public void setSalary(int salary) {
-		this.salary = salary;
-	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", gender=" + gender + ", id=" + id + "]";
+	}
+
 }
-	
