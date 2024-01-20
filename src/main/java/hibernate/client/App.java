9a5b4a54a34739c3e4d646eb325eabacbcacff75
dinesh kumar <dinesh983892@gmail.com>
2024-01-20 12:34:28 +0530

    update

--- src/main/java/hibernate/client/App.java
+++ src/main/java/hibernate/client/App.java
@@ -1,79 +1,68 @@
 package hibernate.client;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import java.util.List;
 
 import hibernate.entity.Address;
 import hibernate.entity.Employee;
 
 public class App {
 
-	public static void main(String[] args) {
-		System.out.println(">>>>>>>>>>>>>>>>>>");
-		Session session1 = HibernateUtil.getSessionFactory().openSession();
-		System.out.println(">>1>>>>>>>>>>>>>>>");
+		public static void main(String[] args) {
+			System.out.println(">>>>>>>>>>>>>>>>>>");
+			Session session1 = HibernateUtil.getSessionFactory().openSession();
 
-		save(session1);
-//		Employee em =  session1.get(Employee.class, 1);
-//		System.out.println(em);
-//		System.out.println(em.address);
-//		Address add = (Address) session1.get(Address.class, 2);
-//		System.out.println(add);
-//		System.out.println(add.employee);
+			save(session1);
 
-//		fetchAllEmployees(session1);
-//		fetchAllAddress(session1);
+//			Employee em = session1.get(Employee.class, 1);
+//			System.out.println("employee:  " + em);
+//			em.getAddress().forEach(a -> {
+//				System.out.println("address:  " + a);
+//			});
+//	
+//			Address add = (Address) session1.get(Address.class, 2);
+//			System.out.println("addd..." + add);
+//			System.out.println("add_emp" + add.getEmployee());
+			session1.close();
+			System.out.println("<<<<<<<<<<<");
+		}
 
-		System.out.println("..............Close Session .............");
-		session1.close();
-//		session2.close();
-		System.out.println("<<<<<<<<<<<");
-	}
+		private static void save(Session session) {
+			Transaction transaction = session.getTransaction();
+			transaction.begin();
+			Employee e1 = new Employee();
 
-//	@SuppressWarnings("deprecation")
-//	private static void fetchAllEmployees(Session session) {
-//		System.out.println("...............Employees Fetching...........");
-//		List<Employee> resultList = session.createQuery("From Employee", Employee.class).getResultList();
-//		for (Employee employee : resultList) {
-//			System.out.println(employee);
-//		}
-////		System.out.println(resultList.get(1).getAddress());
-//		System.out.println("...............Employees end...........");
-//	}
-//
-//	@SuppressWarnings("deprecation")
-//	private static void fetchAllAddress(Session session) {
-//		System.out.println("...............Address Fetching..........");
-//		List<Address> resultList = session.createQuery("From Address", Address.class).getResultList();
-//		for (Address add : resultList) {
-//			System.out.print(add+"     "+add.getEmployee());
-//		}
-////		System.out.println(resultList.get(1).employee);
-//		System.out.println("...............Address end...........");
-//	}
+			e1.setName("Beenu");
+			e1.setGender("male");
+			Address a1 = new Address("h-430", "Sector 62");
+			Address a2 = new Address("h-43", "Sector 63");
+			Address a3 = new Address("h-4", "Sector 64");
+			Address a4 = new Address("h-40", "Sector 65");
+			Address a5 = new Address("h-410", "Sector 66");
+			a1.setEmployee(e1);
+			a2.setEmployee(e1);
+			a3.setEmployee(e1);
+			a4.setEmployee(e1);
+			a5.setEmployee(e1);
+////			session.persist(a1);
+////			session.persist(a2); 
+////			session.persist(a3);
+////			session.persist(a4);
+////			session.persist(a5);
+			List<Address> ad = new ArrayList<>();
+			ad.add(a1);
+			ad.add(a2);
+			ad.add(a3);
+			ad.add(a4);
+			ad.add(a5);
 
-	private static void save(Session session) {
-		Transaction transaction = session.getTransaction();
-		transaction.begin();
-		Employee e = new Employee();
-		e.setFirstName("ram");
-		e.setLastName("dev");
-		Address address = new Address("MP", "ST");
-//		address.employee = e;
-		e.setAddress(address);
-		address.setEmployee(e);
-		session.persist(address);
-		session.persist(e);
-//		Employee e1 = new Employee();
-//		e1.setFirstName("Mihir");
-//		e1.setLastName("binoli");
-//		Address address1 = new Address("lucknow", "Kanpur");
-//		address1.employee = e1;
-//		e1.setAddress(address1);;
+			e1.setAddress(ad);
 
-//		session.persist(address1);
-//		session.persist(e1);
-		transaction.commit();
-	}
+			session.persist(e1);
+			transaction.commit();
+		}
 }
+
 
--- src/main/java/hibernate/entity/Address.java
+++ src/main/java/hibernate/entity/Address.java
@@ -8,65 +8,67 @@ import jakarta.persistence.OneToOne;
 
 
 	
-	@Entity
-	public class Address {
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private int id;
-		public String street;
-		public String city;
-		
-//		@OneToOne
-		@OneToOne( mappedBy = "address")
-		public Employee employee;
-		
-		public Employee getEmployee() {
-			return employee;
-		}
+//@Entity(name ="address2")
+public class Address {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	public String street;
+	public String city;
+	
+//	@ManyToOne
+	public Employee employee;
+	
+	public Address() {
+	}
 
-		public void setEmployee(Employee employee) {
-			this.employee = employee;
-		}
+	
 
-		public Address() {
-		}
+	public Address( String street, String city) {
+		super();
+		this.street = street;
+		this.city = city;
+	}
 
-		public Address(String street, String city) {
-			super();
-			this.street = street;
-			this.city = city;
-		}
 
-		public int getId() {
-			return id;
-		}
 
-		public void setId(int id) {
-			this.id = id;
-		}
+	public int getId() {
+		return id;
+	}
 
-		public String getStreet() {
-			return street;
-		}
+	public void setId(int id) {
+		this.id = id;
+	}
 
-		public void setStreet(String street) {
-			this.street = street;
-		}
+	public String getStreet() {
+		return street;
+	}
 
-		public String getCity() {
-			return city;
-		}
+	public void setStreet(String street) {
+		this.street = street;
+	}
 
-		public void setCity(String city) {
-			this.city = city;
-		}
+	public String getCity() {
+		return city;
+	}
 
-		
+	public void setCity(String city) {
+		this.city = city;
+	}
 
-		@Override
-		public String toString() {
-			return "Address [id=" + id + ", street=" + street + ", city=" + city + "]";
-		}
+	public Employee getEmployee() {
+		return employee;
+	}
 
-		
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", street=" + street + ", city=" + city + "]";
+	}
+
+	
+	
 }
